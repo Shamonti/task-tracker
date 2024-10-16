872fdbfe07cbe05c7cb07922016cb2f308240d06
Shamonti <shamonti.haque98@gmail.com>
2024-10-16 18:28:13 +0600

    Added the model

--- task_tracker/tasks/models.py
+++ task_tracker/tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tasks(models.Model):
+  task_type = models.CharField(max_length=100),
+  completed_by = models.CharField(max_length=50),
+  completed_on = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+        return self.id
